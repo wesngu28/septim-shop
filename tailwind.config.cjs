5356cc39e5f53549f009d04c7bc2219ed243522a
Wesley <60171023+wesngu28@users.noreply.github.com>
2023-03-08 19:06:46 -0800

    write some data with prisma

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 const config = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "fjord": "Fjord One"
+      }
+    },
   },
   plugins: [],
 };
